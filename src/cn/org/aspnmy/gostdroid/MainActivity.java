e45ef3495cbab8c6b304cb6523a145decd29ac53
aspnmy <support@e2bank.cn>
2025-01-02 14:06:41 +0800

    更新dev分支

--- src/cn/org/aspnmy/gostdroid/MainActivity.java
+++ src/cn/org/aspnmy/gostdroid/MainActivity.java
@@ -1,4 +1,4 @@
-package cn.org.rankaiyx.gostdroid;
+package cn.org.aspnmy.gostdroid;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
 
-import cn.org.rankaiyx.gostdroid.R;
+import cn.org.aspnmy.gostdroid.R;
 import android.content.Context;
 
 import android.os.Bundle;
